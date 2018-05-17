96208b41dd3fed868de7705e3f4d3d6812e3f990
张书孟 <zhang_shu_meng@163.com>
2018-05-17 10:31:59 +0800

    [commit]: 新建工程

---
+++ OCTools/OCTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTools
+//
+//  Created by 张书孟 on 2018/5/17.
+//  Copyright © 2018年 ZSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCTools/OCTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCTools
+//
+//  Created by 张书孟 on 2018/5/17.
+//  Copyright © 2018年 ZSM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
